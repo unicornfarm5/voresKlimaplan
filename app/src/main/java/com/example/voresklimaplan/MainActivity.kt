61eba460b666098dcac2e530000da8e4110ba1b6
Dr. Clocktor <jonas@sakejo.dk>
2025-05-16 14:57:50 +0200

    All UI elements on ClassScore has been added

--- app/src/main/java/com/example/voresklimaplan/MainActivity.kt
+++ app/src/main/java/com/example/voresklimaplan/MainActivity.kt
@@ -4,19 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.lifecycleScope
-import com.example.voresklimaplan.data.FirestoreRepository
 import com.example.voresklimaplan.ui.theme.VoresKlimaplanTheme
-import com.example.voresklimaplan.ui.viewModel.ClassesViewModel
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,19 +13,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             VoresKlimaplanTheme {
-                Scoreboard(classesViewModel = ClassesViewModel())
+
             }
         }
     }
 }
 
-@Composable
-fun Scoreboard(classesViewModel: ClassesViewModel) {
-    classesViewModel.getAllClasses()
-}
 
 
-suspend fun getClasses() {
-    val getClassroom = FirestoreRepository()
-    println(getClassroom.getClassroom())
-}
+
+
