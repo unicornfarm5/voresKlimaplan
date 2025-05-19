cd92b045fdbe5ec9c6df98a56170ecac8497173c
F0001 <Faprao.reaungsri@gmail.com>
2025-05-19 15:25:28 +0200

    Added GestureUtil + MainScreen.kt--> pointer events

--- app/src/main/java/com/example/voresklimaplan/game/MainScreen.kt
+++ app/src/main/java/com/example/voresklimaplan/game/MainScreen.kt
@@ -1,24 +1,64 @@
 package com.example.voresklimaplan.game
 
 import android.media.Image
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.navigation.NavController
+import com.example.voresklimaplan.R
+import com.example.voresklimaplan.common.TextFontGaming
+import com.example.voresklimaplan.game.domain.Game
+import com.example.voresklimaplan.game.domain.MoveDirection
+import com.example.voresklimaplan.game.util.detectMoveGesture
+import com.example.voresklimaplan.ui.viewModel.ClassesViewModel
 
-/*
+
+//Føen
 @Composable
-fun MainScreen(modifier: Modifier)
-{
-    Box(
-        modifier = Modifier.fillMaxSize()
+fun MainScreen(
+    navController: NavController,
+    viewModel: ClassesViewModel
 ) {
-        Image(
-            painterResource(Res.drawable.
+    val classList = viewModel.classList //Henter listen fra ViewModel...
+    val game = remember { Game() }
+    var moveDirection by remember{mutableStateOf(MoveDirection.None) }
 
-            )
+
+    TextFontGaming("Davs her er et spil", 20)
+    TextFontGaming(classList[0].score.toString(), 20,  align = TextAlign.Center)
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .pointerInput(Unit){
+                awaitPointerEventScope {
+                    detectMoveGesture(
+                        gameStatus = game.status,
+                        onLeft = {},
+                        onRight = {},
+                        onFingerLifted = {}
+                    )
+                }
+            }
+    ) {
+        Image(
+            painter = painterResource(R.drawable.background_game),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
         )
+
+
     }
 }
-*/
 
--- app/src/main/java/com/example/voresklimaplan/game/util/GestureUtil.kt
+++ app/src/main/java/com/example/voresklimaplan/game/util/GestureUtil.kt
@@ -1,4 +1,47 @@
 package com.example.voresklimaplan.game.util
 
-class GestureUtil {
+import androidx.compose.ui.input.pointer.AwaitPointerEventScope
+import com.example.voresklimaplan.game.domain.GameStatus
+
+//Føen
+suspend fun AwaitPointerEventScope.detectMoveGesture(
+    gameStatus: GameStatus,
+    onLeft:() -> Unit,
+    onRight:()-> Unit,
+    onFingerLifted:() -> Unit,
+    ){
+    while (gameStatus == GameStatus.Started){
+        val downEvent = awaitPointerEvent()
+        val initialDown = downEvent.changes.firstOrNull { it.pressed}
+        if(initialDown == null) continue
+
+        val primaryPointerId = initialDown.id
+        var previousPosition = initialDown.position
+
+        while(true){
+            val event = awaitPointerEvent()
+            val change = event.changes.firstOrNull{
+                it.id == primaryPointerId
+            }
+
+            if(change == null ||!change.pressed){
+                onFingerLifted()
+                break
+            }
+
+            val currentPosition = change.position
+            val deltaX = currentPosition.x - previousPosition.x
+
+            if (deltaX < 0 ){
+                onLeft()
+            } else if(deltaX > 0 ){
+                onRight()
+            }
+
+            previousPosition = currentPosition
+            change.consume()
+        }
+    }
+
+
 }
