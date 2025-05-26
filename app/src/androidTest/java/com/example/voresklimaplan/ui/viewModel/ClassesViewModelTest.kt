de8fd398ef0bd848cbeb4344e83d5751bebf6427
nikoleta <nikosafa7895@gmail.com>
2025-05-26 14:50:52 +0200

    Coding the first Unit test succesfully

--- app/src/androidTest/java/com/example/voresklimaplan/ui/viewModel/ClassesViewModelTest.kt
+++ app/src/androidTest/java/com/example/voresklimaplan/ui/viewModel/ClassesViewModelTest.kt
@@ -8,28 +8,55 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.tasks.await
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.UUID
 
 @RunWith(AndroidJUnit4::class)
 class ClassesViewModelTest {
 
-//Linea og Nikoleta
+    // Linea og Nikoleta
     @Test
     fun fetchAllClassrooms_successfully() = runBlocking {
-        //Giver tilladelse fra Firebase
+        // Giver tilladelse fra Firebase
         val context = InstrumentationRegistry.getInstrumentation().targetContext
         val firestore = FirebaseFirestore.getInstance()
 
-        // Hent alle dokumenter fra "Classroom"-samlingen
+        // Hent alle dokumenter fra "Classroom"
         val snapshot = firestore.collection("Classroom").get().await()
 
         // Google Truth assertions
         assertThat(snapshot).isNotNull()
         assertThat(snapshot.isEmpty).isFalse()
 
-        // Udskriv hver klasse
+        // Udskriver hver klasse
         for (doc in snapshot.documents) {
             println("Classroom: ${doc.id} => ${doc.data}")
             assertThat(doc.data).isNotEmpty()
         }
+    }
+
+    // Linea og Nikoleta
+    @Test
+    fun pushScoreToFirestore_successfully() = runBlocking {
+        // Giver tilladelse fra Firebase
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        val firestore = FirebaseFirestore.getInstance()
+
+        // Giver testen et unikt ID
+        val userId = UUID.randomUUID().toString()
+        val testScore = 100
+
+        // Upload til score i firebase
+        val data = hashMapOf("score" to testScore)
+        firestore.collection("scores").document(userId).set(data).await()
+
+        // Hent den igen for at se at det virkede
+        val snapshot = firestore.collection("scores").document(userId).get().await()
+
+        // Google Truth checks
+        assertThat(snapshot.exists()).isTrue()
+        assertThat(snapshot.getLong("score")?.toInt()).isEqualTo(testScore)
+
+        println("Score uploaded and verified: $testScore for user $userId")
+
     }
 }
